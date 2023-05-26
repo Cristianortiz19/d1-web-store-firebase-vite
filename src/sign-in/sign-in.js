28dd709ada400aa77afe23c8d41b06990d771529
Cristian Ortiz <cristclar012@gmai.com>
2023-05-26 08:24:57 -0500

    css

--- src/sign-in/sign-in.js
+++ src/sign-in/sign-in.js
@@ -8,12 +8,11 @@ async function signUp() {
     const email = document.getElementById('email-input').value
     const pass = document.getElementById('pass-input').value
     const confPass = document.getElementById('confirm-pass-input').value
-    const image = document.getElementById('profile-img').files[0]
     const username = document.getElementById('username-input').value
 
     if (pass !== confPass) alert('Las contraseñas no coinciden')
     else {
-        const userCreated = await createUser(email, pass, username, image)
+        const userCreated = await createUser(email, pass, username)
         if (userCreated.status) {
             alert('usuario creado con exito, uid: ' + userCreated.info)
         } else {
